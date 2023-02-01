a1838e997cca7ea431285309daffb609a2dccae4
sahilgupta-srm <113188398+sahilgupta-srm@users.noreply.github.com>
2023-02-01 18:10:05 +0530

    Fixed a small error

--- server.py
+++ server.py
@@ -5,6 +5,6 @@ s.bind((socket.gethostname(),5050))#5050 is the port number
 s.listen(5)#To make a queue
 while True:
     cst,add=s.accept()#cst stores object,add is their IP address
-    print(f"Connectoin from {address} has been established")
-    clientsocket.send(bytes("Welcome to the server!","utf-8"))#for sending information
-    clientsocket.close()
+    print(f"Connection from {add} has been established")
+    cst.send(bytes("Welcome to the server!","utf-8"))#for sending information
+    cst.close()
